c4b7c99468e32ea0ab9f7ea69cb7a2d805691dc3
mrk-shahzada <mrktechnicalsolutions@gmail.com>
2026-05-26 00:05:04 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.jsx", // Added this just in case
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
